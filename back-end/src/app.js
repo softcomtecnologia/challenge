7fa4e5b7f685dfa5080ac4f45bf22a8ebe8a374b
Thalysson <thalyssonwg.nascimento@gmail.com>
2020-01-09 08:26:16 -0300

    consfiguration eslint

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,18 +2,18 @@ import express from 'express';
 import routes from './routes';
 
 class App {
-    constructor(){
+    constructor() {
         this.server = express();
-        
+
         this.middlewares();
         this.routes();
     }
 
-    middlewares(){
-        this.server.use(express.json()); 
+    middlewares() {
+        this.server.use(express.json());
     }
 
-    routes(){
+    routes() {
         this.server.use(routes);
     }
 }
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -2,8 +2,6 @@ import Router from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ok: true})
-})
+routes.get('/', (req, res) => res.json({ ok: true }));
 
 export default routes;
