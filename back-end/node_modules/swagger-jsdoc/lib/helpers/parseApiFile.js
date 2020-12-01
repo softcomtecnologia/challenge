70584d77ac926f42be4c4e0fb49f72aa04120c1c
Pedro Henrique <pedrinhoas7@gmail.com>
2020-11-30 23:28:40 -0300

    add php

---
+++ back-end/node_modules/swagger-jsdoc/lib/helpers/parseApiFile.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const parseApiFileContent = require('./parseApiFileContent');
+/**
+ * Parses the provided API file for JSDoc comments.
+ * @function
+ * @param {string} file - File to be parsed
+ * @returns {{jsdoc: array, yaml: array}} JSDoc comments and Yaml files
+ */
+function parseApiFile(file) {
+  const fileContent = fs.readFileSync(file, { encoding: 'utf8' });
+  const ext = path.extname(file);
+
+  return parseApiFileContent(fileContent, ext);
+}
+
+module.exports = parseApiFile;
