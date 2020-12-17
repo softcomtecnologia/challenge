f77b05bbce1bd1966454577f626e09103c86ed95
Pedro Henrique <pedrinhoas7@gmail.com>
2020-12-16 22:50:47 -0300

    recommit

---
+++ php-challenge/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+    <script src="js/app.js"></script>
+    </head>
+    <body class="antialiased">
+        
+       
+                <Example></Example>
+        
+
+    </body>
+</html>
