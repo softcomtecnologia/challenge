be9e621e619df040e9edbd642e75296e314a6d2f
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-05-15 14:47:20 -0300

    add formik+yup validation to search input field and button

--- front-end/pet-shop/src/components/MainSearchInput.jsx
+++ front-end/pet-shop/src/components/MainSearchInput.jsx
@@ -1,24 +1,85 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 import React from 'react';
-import { InputGroup, InputGroupAddon, InputGroupText, Input, Button } from 'reactstrap';
+import { connect } from 'react-redux';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { InputGroup, InputGroupAddon, InputGroupText, Input, Button,
+  Form, FormFeedback } from 'reactstrap';
+import { handleInput } from '../store/actions/search';
+
 import search from '../assets/Search.svg';
 
 import './MainSearchInput.style.css';
 
-const MainSearchInput = () => (
-  <div className="input-container">
-    <br />
-    <InputGroup>
-      <Input placeholder="O que você procura?" />
-      <InputGroupAddon addonType="prepend">
-        <InputGroupText><img src={ search } alt="search" /></InputGroupText>
-      </InputGroupAddon>
-    </InputGroup>
-    <br />
-    <InputGroupAddon addonType="append">
-      <Button color="secondary">Pesquisar</Button>
-    </InputGroupAddon>
-    <br />
-  </div>
-);
+const initialValues = {
+  queryInput: '',
+};
 
-export default MainSearchInput;
+// creating the validation schema
+const validationSchema = yup.object().shape({
+  queryInput: yup
+    .string()
+    .required('É necessário um termo para busca')
+    .matches(/^[aA-zZ\s]+$/, 'Somente letras de A a Z')
+    .min(2, 'Pelo menos duas letras'),
+});
+
+const MainSearchInput = ({ onSubmit, handleQuery }) => {
+  // using useFormik
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+  // use formik.getFieldProps for input fields
+  const queryProps = formik.getFieldProps('queryInput');
+
+  const { name, onBlur, onChange, value } = queryProps;
+
+  return (
+    <div className="input-container">
+      <br />
+      <Form>
+        <InputGroup>
+          <Input
+            invalid={ formik.touched.queryInput && formik.errors.queryInput }
+            name={ name }
+            onBlur={ onBlur }
+            value={ value }
+            onChange={ onChange }
+            placeholder="O que você procura?"
+          />
+          <InputGroupAddon addonType="prepend">
+            <InputGroupText><img src={ search } alt="search" /></InputGroupText>
+          </InputGroupAddon>
+          {formik.touched.queryInput && formik.errors.queryInput ? (
+            <FormFeedback>{formik.errors.queryInput}</FormFeedback>
+          ) : null}
+        </InputGroup>
+        <br />
+        <InputGroupAddon addonType="append">
+          <Button
+            color="secondary"
+            disabled={ !(formik.isValid && formik.dirty) }
+            // type="submit"
+            onClick={ () => handleQuery(value) }
+          >
+            Pesquisar
+          </Button>
+        </InputGroupAddon>
+      </Form>
+      <br />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  inputQuery: state.inputQuery,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  handleQuery: (queryInput) => dispatch(handleInput(queryInput)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainSearchInput);
