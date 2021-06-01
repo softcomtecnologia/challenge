545dbc2aba2a8647e3b50168fbb3b921be560faf
devFelipeFranca <felipefrancafj@gmail.com>
2021-06-01 15:27:30 -0300

    Done CardProductMap

---
+++ front-end/petshop/components/CardProductMap/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+import Card from "./Card";
+
+import data from "../../mock/index.json";
+
+import * as S from "./styles";
+
+export default function CardProductMap({
+  categories = data.categories,
+  sections = data.sections,
+}) {
+  const history = useHistory();
+  function handleShowDetails({ target }) {
+    const { id } = target;
+    history.push(`/productDetails/${id}`);
+  }
+  return (
+    <>
+      {categories.map((category) => (
+        <>
+          <S.CategoryMap>{category}</S.CategoryMap>
+          <S.ProductsContainer>
+            <Card
+              handleShowDetails={handleShowDetails}
+              products={sections[category].products}
+            />
+          </S.ProductsContainer>
+        </>
+      ))}
+    </>
+  );
+}
