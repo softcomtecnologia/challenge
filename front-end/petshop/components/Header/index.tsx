1a5be70594a3c5634b94b95b6c28b40a0d5f1460
devFelipeFranca <felipefrancafj@gmail.com>
2021-06-01 13:55:50 -0300

    Done Header Component

--- front-end/petshop/components/Header/index.tsx
+++ front-end/petshop/components/Header/index.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 
 import * as S from "./styles";
 
-import Logo from "../../assets/capas-de-cachorro-fofo-1 1.png"
-
 export default function Header({ disableDetailsScreen }) {
+  const history = useHistory();
+  function handleBack() {
+    history.push("/")
+  }
   return (
     <>
       {disableDetailsScreen ? (
         <S.Header>
-          <S.HeaderImage alt="Imagem Logo da Empresa" src={Logo} />
+          <S.HeaderImage alt="Imagem Logo da Empresa" src="/images/capas-de-cachorro-fofo-1 1.png" />
           <S.HeaderContainer>
             <S.HeaderTitle>Pet Friends Acessories</S.HeaderTitle>
-            <S.HeaderClockIcon src="" />
+            <S.HeaderClockIcon src="/images/clock.svg" />
             <S.HeaderStatus>Aberto Agora</S.HeaderStatus>
             <S.HeaderStreet>
               Avenida Rio Grande do Sul, 1520, Estados | 58030-021 | João Pessoa
               - PB
             </S.HeaderStreet>
-            <img alt="" src="" />
+            <img alt="" src="/images/moto.svg" />
             <S.HeaderService>Delivery: </S.HeaderService>
             <S.HeaderServiceTime>35m - 1h:40m</S.HeaderServiceTime>
             <S.HeaderService>Entrega: </S.HeaderService>
@@ -33,9 +37,9 @@ export default function Header({ disableDetailsScreen }) {
         </S.Header>
       ) : (
         <S.HeaderDetails>
-          {/* <S.BackBtn onClick={handleBack}>
-            <img src={BackBtn} />
-          </S.BackBtn> */}
+          <S.BackBtn onClick={handleBack}>
+            <img src="/images/backbtn.png" />
+          </S.BackBtn>
         </S.HeaderDetails>
       )}
     </>
--- front-end/petshop/pages/index.tsx
+++ front-end/petshop/pages/index.tsx
@@ -1,13 +1,12 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { GlobalStyle } from "../styles/globals";
 
 export default function Home() {
   return (
-    <GlobalStyle>
-      <Header disableDetailsScreen={true} />
+    <>
+      <Header disableDetailsScreen={true}  />
       Hello Challenge Softcom
       {/* <Footer /> */}
-    </GlobalStyle>
+    </>
   );
 }
