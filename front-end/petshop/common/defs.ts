b692eb7d124e3425cbcedeb9c6e5cd59ae8df006
devFelipeFranca <felipefrancafj@gmail.com>
2021-06-01 14:42:01 -0300

    Update Home Page With NavBar And SearchBar Component

---
+++ front-end/petshop/common/defs.ts
@@ -0,0 +1,8 @@
+export const CATEGORIES = [
+  "Sugestão do Vendendor",
+  "Brinquedos",
+  "Camas e Casinhas",
+  "Coleiras",
+  "Ossos e Petiscos",
+  "Saúde",
+];
--- front-end/petshop/pages/index.tsx
+++ front-end/petshop/pages/index.tsx
@@ -1,12 +1,13 @@
-import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Navbar from "../components/NavBar";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
     <>
-      <Header disableDetailsScreen={true}  />
-      Hello Challenge Softcom
-      {/* <Footer /> */}
+      <Header disableDetailsScreen={true} />
+      <Navbar />
+      <SearchBar />
     </>
   );
 }
